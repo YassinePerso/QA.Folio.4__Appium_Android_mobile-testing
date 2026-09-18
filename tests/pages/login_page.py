fb5d876a5c04bf4205760a1dc1557c32bc177398
Yass <yassineboulakhrif75@gmail.com>
2026-09-18 23:53:44 +0200

    FEAT: login_page file refactored

--- tests/pages/login_page.py
+++ tests/pages/login_page.py
@@ -2,13 +2,20 @@ from .base_page import BasePage
 
 
 class LoginPage(BasePage):
+    MENU_BUTTON = "com.saucelabs.mydemoapp.android:id/menuIV"
+    LOGIN_MENU_ITEM = "Login Menu Item"
     USERNAME_FIELD = "com.saucelabs.mydemoapp.android:id/nameET"
     PASSWORD_FIELD = "com.saucelabs.mydemoapp.android:id/passwordET"
     LOGIN_BUTTON = "com.saucelabs.mydemoapp.android:id/loginBtn"
 
+    #Navigue vers la page loginS
+    def navigate_to_login(self):
+        self.click(self.MENU_BUTTON)
+        self.click_by_acc_id(self.LOGIN_MENU_ITEM)
 
-    #Action complète pour se connecter à l'application
+    #Algo qui permet l'action complète de login
     def login(self, username, password):
+        self.navigate_to_login()
         self.type_text(self.USERNAME_FIELD, username)
         self.type_text(self.PASSWORD_FIELD, password)
         self.click(self.LOGIN_BUTTON)
